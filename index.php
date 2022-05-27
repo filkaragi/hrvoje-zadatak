2ad55b2a39fa1a19014096f6eadb0ebd96fda528
Crouch <filip.k@euroart93.hr>
2022-05-27 08:51:17 +0200

    fc

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+    <head>
+        <meta name="Prvi projekt" content="Naslovi">
+        <title>Prvi naslov</title>
+</head>
+<body>
+    <header>
+    <h1> 1. Naslov </h1>
+</header>
+        <p> prvi odlomak </p>
+        <h2> 1. Podnaslov </h2>
+        <p> Bivša hrvatska pjevačica i voditeljica Vlatka Pokos
+        ponovno je zagolicala maštu svojih fanova nakon što
+        je objavila svoju golu 
+        fotografiju na Instagramu. Riječ je o slavnom aktu
+        koji je 1994. godine snimio Stephan Lupino.
+    <a href=https://www.instagram.com/p/CeAwSEOsreM/?utm_source=ig_embed&ig_rid=2e32e343-65a3-49fa-81a8-bab51de812a3">
+    KLIKNI</a></p>
+        <h3> 2. Podnaslov </h3>
+        <p> treći odlomak </p>
+    <img src="https://www.tportal.hr/media/thumbnail/900x540/1210977.jpeg?cropId=1272912"
+    alt="Zdravko u elementu"
+style="width:200px; height:200px;">
+<footer>
+    <adress>
+        Napisao <a href="mailto:lepesichrvoje@gmail.com">Lepi</a>
+</footer>
+</body>
+</html>
